0f70d91f7eee9958c47acf8569b818af91319484
maldonado20 <otonel2002malzanado@gmail.com>
2025-10-24 11:56:09 -0600

    "Se implemento la logica para registro de usuario, login y cierre de sección"

--- app/src/main/java/com/dsm441/lecturadigital/MainActivity.kt
+++ app/src/main/java/com/dsm441/lecturadigital/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.dsm441.lecturadigital
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+
+    //declaramos las variables firebase y las vistas
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btnLogout: Button
+    private lateinit var tvWelcomeUser: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        auth = Firebase.auth
+
+        //inicializar vistas
+        btnLogout = findViewById(R.id.btnLogout)
+        tvWelcomeUser = findViewById(R.id.tvWelcomeUser)
+
+
+        val currentUser: FirebaseUser? = auth.currentUser
+        if (currentUser != null) {
+            tvWelcomeUser.text = "Bienvenido, ${currentUser.email}"
+        }else {
+            goToLoginActivity()
+        }
+
+        //configurar el Click listener de Logout
+        btnLogout.setOnClickListener {
+            signOut()
         }
     }
+
+    //funcion para salir de la sesion
+    private fun signOut(){
+        auth.signOut()
+        goToLoginActivity()
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish() //Destruimos la actividad para que el usuario no pueda volver atras
+    }
+
 }
